fd0d2880af5ca6f7771b2f6b610053b2dcb831e3
Francesc Ara <cesc78@gmail.com>
2020-03-22 20:08:08 +0100

    Added tests

---
+++ tests/blogs.test.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  let blogObject = new Blog(helper.initialBlogs[0])
+  await blogObject.save()
+
+  blogObject = new Blog(helper.initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body.length).toBe(2)
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = helper.initialBlogs[2]
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd.length).toBe(3)
+
+  const title = blogsAtEnd.map(b => b.title)
+  expect(title).toContain(newBlog.title)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
